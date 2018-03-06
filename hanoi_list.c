5cf5b459bca7b94d5b9860a9fe56f7c4ca043d97
usuario <usuario@LAPTOP-RF1KMOL3>
2018-03-07 00:48:36 +0100

    He fet un part del tema de llistes dinamiques

--- hanoi_list.c
+++ hanoi_list.c
@@ -13,3 +13,40 @@
  * Estat de l'entrega: 
  * Altres comentaris o incidencies:
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "hanoi_list.h"
+
+void agregar_moviment(node* _node) {
+
+    _node->seguent = NULL;
+    
+    if (primer == NULL) {
+        primer = _node;
+        ultim = _node;
+    } else {
+        ultim->seguent = _node;
+        ultim = _node;
+    }
+
+}
+
+int mostrar_moviment() {
+
+    
+    int i;
+    for(i=0; i<MOVIMENTS; i++){
+        node* Elemento_lista = malloc(sizeof (node));
+        Elemento_lista->moviment = "%d Moviment";
+        
+        agregar_moviment(Elemento_lista);
+    }
+        node* i;
+        
+        while(i != NULL){
+            printf("%s\n", i->moviment);
+            i = i->seguent;
+        }
+    
+}
